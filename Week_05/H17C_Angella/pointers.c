fece8f75f3a3a3f79795cfa296dbe94eabdcf622
anjerraa <angellapham240@gmail.com>
2022-06-30 18:07:19 +1000

    Codes for H17C tut - Angella

---
+++ Week_05/H17C_Angella/pointers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void) {
+
+    // Create an int variable;
+    // Address 0xFF00
+    int var = 2;
+
+    // Creating an int pointer -> ptr;
+    int *ptr;
+    
+    // Getting the address of a variable;
+    ptr = &var;
+
+    // Print the address that ptr stores;
+    // Prints out 0xFF00 (which is the address of
+    // var).
+    printf("Address that ptr stores: %p\n", ptr);
+
+    // Accessing memory via a pointer;
+    // Prints out the value of var which is 2.
+    printf("Value stored at the address in ptr: %d\n", *ptr);
+
+    // Change the value stored at the address.
+    *ptr = 22;
+
+    printf("The value of var: %d\n", var);
+
+    return 0;
+}
