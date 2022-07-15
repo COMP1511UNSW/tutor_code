04fff62e048e1bdde5a5292a0cb049f8c95d11e6
Rory Golledge <r.golledge@unsw.edu.au>
2022-07-15 14:21:31 +1000

    Add F13A code

---
+++ Week_07/F13A-Rory/fgets_loop.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+#define MAX_STR_SIZE 1024
+
+int main(void) {
+    char string[MAX_STR_SIZE];
+    while (fgets(string, MAX_STR_SIZE, stdin) != NULL) {
+        printf("%s", string);
+    }
+    return 0;
+}
+
---
+++ Week_07/F13A-Rory/strings.c
@@ -0,0 +1,51 @@
+
+#include <stdio.h>
+
+// Manually prints out a string, one character at a time.
+// Should behave like printf("%s\n");
+void print_string(char *string) {
+    int i = 0;
+    while (string[i] != '\0') {
+        printf("%c", string[i]);
+        i++;
+    }
+    printf("\n");
+}
+
+
+
+int main (void) {
+    // These all store the same string
+    // char string1[100] = {'h', 'e', 'l', 'l', 'o', '\n', '\0'};
+    // char string2[100] = "hello\n";
+    // char string3[] = "hello\n";
+    // char *string4 = "hello\n";
+    
+    char string[] = "Hello \0world!\n";
+    print_string(string);
+    printf("%s", string);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
